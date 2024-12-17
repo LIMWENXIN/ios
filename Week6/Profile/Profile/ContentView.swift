275023b1707379c632be9283e64708fff72412d4
LIM WEN XIN <limwenxin0923e.neivce.edu.my>
2024-12-17 18:21:14 +0800

    Profile Exe Done

---
+++ Week6/Profile/Profile/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Profile
+//
+//  Created by labuser on 17/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .top){
+            Image("background")
+                .resizable()
+                .frame(height: UIScreen.main.bounds.height/4)
+                .ignoresSafeArea()
+            VStack{
+                Image("LWX")
+                    .resizable()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .scaledToFit()
+                    .aspectRatio(contentMode: .fit)
+                    .overlay(Circle().stroke(Color.white,lineWidth:4))
+                    .padding(.all)
+                    .frame(width: 250, height: 250)
+                Text("LIM WEN XIN")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text("IOS Developer")
+                    .font(.title)
+                Text("ACSS is a digital agency company and website studio specializing in products, E-commerce, and SaaS website.")
+                    .multilineTextAlignment(.center)
+                    .padding(.all)
+                    .frame(width: 400, height:100)
+                Spacer()
+                HStack{
+                    Image("IG")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Image("facebook")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Image("Linkedln")
+                        .resizable()
+                        .frame(width: 70, height: 70)
+                    Image("tiktok")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
