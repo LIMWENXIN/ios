ab8f565037dbca406ff652e6d8c8994022bcb9c2
LIM WEN XIN <limwenxin0923e.neivce.edu.my>
2024-12-18 11:26:01 +0800

    Week 7 (Favourite Artists)

---
+++ Week7/Artist Profile/Artist Profile/Artist_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artist_ProfileApp.swift
+//  Artist Profile
+//
+//  Created by labuser on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Artist_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
