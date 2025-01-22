cdd796a91016bcdb588ede9049047a86ce401b77
LIM WEN XIN <limwenxin0923e.neivce.edu.my>
2025-01-22 12:24:38 +0800

    Week 11

--- Week11/7 day Forecast Weather/7 day Forecast Weather/CityWeather.swift
+++ Week11/7 day Forecast Weather/7 day Forecast Weather/CityWeather.swift
@@ -5,4 +5,110 @@
 //  Created by labuser on 22/01/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct CityWeather: Identifiable {
+    let id = UUID()
+    let city: String
+    let weather: [Weather]
+}
+
+struct Weather: Identifiable {
+    let id = UUID()
+    let day: String
+    let icon:String
+    let temperature: Int
+}
+
+let AsiaWeatherData: [CityWeather] = [
+    CityWeather(
+        city: "Kajang",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud",temperature: 32),
+            Weather(day: "THU", icon: "cloud.bolt", temperature: 31),
+            Weather(day: "FRI", icon: "cloud.heavyrain", temperature: 31),
+            Weather(day: "SAT", icon: "cloud.bolt", temperature: 29),
+            Weather(day: "SUN", icon: "cloud", temperature: 32),
+        ]
+    ),
+    CityWeather(
+        city: "Kuala Lumpur",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud",temperature: 32),
+            Weather(day: "THU", icon: "cloud.bolt", temperature: 31),
+            Weather(day: "FRI", icon: "cloud.heavyrain", temperature: 31),
+            Weather(day: "SAT", icon: "cloud", temperature: 30),
+            Weather(day: "SUN", icon: "cloud.sun", temperature: 32),
+        ]
+    ),
+    CityWeather(
+        city: "Shanghai",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud.sun.fill",temperature: 16),
+            Weather(day: "THU", icon: "cloud.sun.fill", temperature: 16),
+            Weather(day: "FRI", icon: "cloud.sun.fill", temperature: 15),
+            Weather(day: "SAT", icon: "cloud.sun.fill", temperature: 17),
+            Weather(day: "SUN", icon: "cloud.sun", temperature: 15),
+        ]
+    )
+]
+
+let EuropeWeatherData: [CityWeather] = [
+    CityWeather(
+        city: "Berlin",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud.sun",temperature: 4),
+            Weather(day: "THU", icon: "cloud.rain", temperature: 6),
+            Weather(day: "FRI", icon: "cloud.sun.fill", temperature: 8),
+            Weather(day: "SAT", icon: "cloud.sun.fill", temperature: 5),
+            Weather(day: "SUN", icon: "cloud.sun", temperature:7),
+        ]
+    ),
+    CityWeather(
+        city: "Strasbourg",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud",temperature: 3),
+            Weather(day: "THU", icon: "cloud.drizzle", temperature: 8),
+            Weather(day: "FRI", icon: "cloud", temperature: 10),
+            Weather(day: "SAT", icon: "cloud", temperature: 3),
+            Weather(day: "SUN", icon: "cloud.sun", temperature: 5),
+        ]
+    ),
+    CityWeather(
+        city: "Dublin",
+        weather: [
+            Weather(day: "TODAY", icon: "cloud.sun.fill",temperature: 6),
+            Weather(day: "THU", icon: "cloud.drizzle", temperature: 6),
+            Weather(day: "FRI", icon: "cloud.sun.fill", temperature: 8),
+            Weather(day: "SAT", icon: "cloud.sun.fill", temperature: 7),
+            Weather(day: "SUN", icon: "cloud.drizzle", temperature: 5),
+        ]
+    )
+]
+
+struct CityWeatherView: View {
+    var cityWeather: CityWeather
+    
+    var body: some View {
+        Text(cityWeather.city)
+            .font(.title)
+            .bold()
+        HStack {
+            ForEach(cityWeather.weather) { weather in
+                VStack {
+                    Text(weather.day)
+                        .font(.headline)
+                    Image(systemName: weather.icon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                    Text("\(weather.temperature)°C")
+                        .font(.subheadline)
+                }
+                .padding(.vertical)
+            }
+        }
+
+        .padding()
+    }
+}
--- Week11/7 day Forecast Weather/7 day Forecast Weather/ContentView.swift
+++ Week11/7 day Forecast Weather/7 day Forecast Weather/ContentView.swift
@@ -6,17 +6,84 @@
 //
 
 import SwiftUI
-
+    
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+    @State private var isSheetPresented = false
+    @State private var searchText = ""
+    @State private var isRed = false
+    
+    let asiacities = AsiaWeatherData
+    let europecities = EuropeWeatherData
+    
+    var asiasearchResults: [CityWeather] {
+        if searchText.isEmpty {
+            return asiacities
+        } else {
+            return asiacities.filter { $0.city.contains(searchText) }
         }
-        .padding()
     }
+    
+    var eurosearchResults: [CityWeather] {
+        if searchText.isEmpty {
+            return europecities
+        } else {
+            return europecities.filter { $0.city.contains(searchText) }
+        }
+    }
+    
+    var body: some View{
+        NavigationStack{
+            List{
+                // Asia
+                Section(header: Text("Asia")){
+                    ForEach(asiasearchResults, id:\.city){ asia in
+                        NavigationLink{
+                            CityWeatherView(cityWeather: asia)
+                            ZStack {
+                                Color.white
+                                    .cornerRadius(10)
+                                    .frame(width: 280, height: 40)
+                                Button("Change Day Time",action: {
+                                    isRed = true
+                                })
+                            }
+                            .backgroundStyle(Color.black)
+                        }label:{
+                            Text(asia.city)
+                        }
+                    }
+                }
+                // Europe
+                Section(header: Text("Europe")){
+                    ForEach(eurosearchResults, id:\.city){ europe in
+                        NavigationLink{
+                            CityWeatherView(cityWeather: europe)
+                        }label:{
+                            Text(europe.city)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Cities")
+            .toolbar{
+                ToolbarItem(placement:.navigationBarTrailing){
+                    Button(action:{
+                        isSheetPresented = true
+                    }){
+                        Image(systemName: "info.circle")
+                    }
+                    .sheet(isPresented: $isSheetPresented) {
+                        Text("This app was created by LIM WEN XIN")
+                            .presentationDetents([.height(200), .large])
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+        }
+    }
+    
+    
 }
 
 #Preview {
